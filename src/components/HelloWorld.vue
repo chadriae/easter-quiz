c5450f792cd4c141afa819a8c49841a188745674
Christophe Adriaensens <christophe@starringjane.com>
2023-04-20 09:40:52 +0200

    Change welcoming message

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,11 @@
 </script>
 
 <template>
-  <div class="mb-12">
+  <div>
     <img class="m-auto w-64 h-auto" src="@/assets/bunny.png" />
 
-    <p class="text-center mt-12">
-      Proficiat! Jullie zijn al zo ver geraakt. Maar eerst toch snel deze crytogram oplossen.
+    <p class="text-center pt-12 pb-6 border-b-2 border-dotted border-pink-300">
+      Proficiat! Jullie zijn al zo ver geraakt. Maar eerst toch snel deze quiz oplossen.
     </p>
   </div>
 </template>

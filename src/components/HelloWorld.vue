9daf91ed7a34ef6bd9d2bff60902114771ca66cb
Christophe Adriaensens <christophe@starringjane.com>
2023-04-21 13:47:11 +0200

    Update instructions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,7 @@
 
     <p class="text-center pt-12 pb-6 border-b-2 border-dotted border-pink-300">
       Proficiat! Jullie zijn al zo ver geraakt. Maar eerst toch snel deze quiz oplossen.
+      Ps: de antwoorden zijn hoofdlettergevoelig.
     </p>
   </div>
 </template>
